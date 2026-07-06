fe09f625495f5f05018d4a557a84626b7294eca3
StealKkak <admin@azura.cfx.kr>
2026-07-06 19:48:18 +0900

    chore(exception): 500 핸들러 예외 콘솔에 로그하도록 수정

--- src/middlewares/InternalServerError.ts
+++ src/middlewares/InternalServerError.ts
@@ -1,6 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 
 export default function InternalServerError(err: any, req: Request, res: Response, next: NextFunction) {
+    console.error(err);
     if (err.code) {
         return res.status(err.code).json({ status: "error", message: err.message });
     }
--- src/middlewares/PageError.ts
+++ src/middlewares/PageError.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 
 export default function PageError(err: any, req: Request, res: Response, next: NextFunction) {
+    console.error(err);
     return res.status(500).render("errors/500.ejs");
 }

bedd6b1d18ee555c0b5973fd9ca4c7c2fca0b9f6
StealKkak <admin@azura.cfx.kr>
2026-07-14 15:11:31 +0900

    feat(view): 네비바 추가

--- src/domains/home/HomeController.ts
+++ src/domains/home/HomeController.ts
@@ -3,7 +3,8 @@ import type { Request, Response } from "express";
 export default class HomeController {
     public static HomePage(req: Request, res: Response) {
         return res.render("index.ejs", {
-            title: "홈"
+            title: "홈",
+            user: req.session.user
         });
     }
 }

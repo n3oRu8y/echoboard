b39ebc8d9727a28204038ae92c0fd90edcc3d1ed
neoruby <admin@azura.cfx.kr>
2026-07-30 01:34:05 +0900

    feat(user): 닉네임 변경 페이지 자바스크립트 연결

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -4,3 +4,30 @@ document.querySelectorAll("#btn-logout").forEach(btn => {
         location.reload();
     });
 });
+
+const nicknameForm = document.getElementById("nickname-form");
+nicknameForm.addEventListener("submit", async e => {
+    e.preventDefault();
+    const formData = new FormData(nicknameForm);
+
+    try {
+        const res = await fetch("/api/users/me", {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                nickname: formData.get("nickname")?.toString().trim()
+            })
+        });
+
+        if (!res.ok) {
+            const data = await res.json();
+            return alert(data.message);
+        }
+
+        return location.href = "/mypage";
+    } catch {
+        alert("인터넷 연결 상태를 확인해주세요.");
+    }
+});

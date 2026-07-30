289a70c26033c817ce3373bc6868d83ddba4fae3
neoruby <admin@azura.cfx.kr>
2026-07-30 17:33:48 +0900

    feat(auth): 2단계 인증 페이지 추가

--- public/js/2fa.js
+++ public/js/2fa.js
@@ -56,3 +56,45 @@ document.querySelectorAll("#two-fa-disable-form").forEach(form => {
         }
     });
 });
+
+document.querySelectorAll("#two-fa-verify-form").forEach(form => {
+    form.addEventListener("submit", async e => {
+        e.preventDefault();
+        const formData = new FormData(form);
+
+        try {
+            const res = await fetch("/api/auth/login/2fa", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    token: formData.get("token")?.toString().trim(),
+                    trustDevice: formData.has("trustDevice")
+                })
+            });
+
+            if (!res.ok) {
+                const data = await res.json();
+
+                const error = document.querySelector("#error-message");
+                error.textContent = data.message ?? "인증에 실패했습니다.";
+                error.classList.remove("d-none");
+
+                return;
+            }
+
+            const redirect = new URLSearchParams(location.search).get("redirect");
+
+            if (redirect && redirect.startsWith("/") && !redirect.startsWith("//")) {
+                location.href = redirect;
+            } else {
+                location.href = "/";
+            }
+        } catch {
+            const error = document.querySelector("#error-message");
+            error.textContent = "인터넷 연결 상태를 확인해주세요.";
+            error.classList.remove("d-none");
+        }
+    });
+});

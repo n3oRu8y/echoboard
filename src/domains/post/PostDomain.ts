75ca2a6a781fe06e4106939a0a95f944f9a10e6f
neoruby <admin@azura.cfx.kr>
2026-08-06 16:33:56 +0900

    feat(post): 게시글 도메인 객체에 게시글 반응 객체를 속성으로 추가

--- src/domains/post/PostDomain.ts
+++ src/domains/post/PostDomain.ts
@@ -1,6 +1,7 @@
 import Attachment from "../attachment/AttachmentDomain.js";
 import Board from "../board/BoardDomain.js";
 import Comment from "../comment/CommentDomain.js";
+import Reaction from "../reaction/ReactionDomain.js";
 import User from "../user/UserDomain.js";
 
 export default class Post {
@@ -19,7 +20,7 @@ export default class Post {
         public board: Board | null,
         
         public attachments: Array<Attachment> | null,
-        public reactions: Array<null> | null, // 나중에 추가
+        public reactions: Array<null>,
 
         public readonly createdAt: Date,
         public updatedAt: Date,
@@ -38,7 +39,7 @@ export default class Post {
             boardId,
             null,
             null,
-            null,
+            [],
             createdAt,
             createdAt,
             null
@@ -57,7 +58,7 @@ export default class Post {
             row.boardId,
             row.board ? Board.FromRow(row.board) : null,
             row.attachments ? row.attachments.map((attachment: any) => Attachment.FromRow(attachment)) : null,
-            null, // 수정 필요
+            row.reactions ? row.reactions.map((reaction: any) => Reaction.FromRow(reaction)) : [],
             row.createdAt,
             row.updatedAt,
             row.deletedAt

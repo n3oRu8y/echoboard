7025e01ac62af3a52d21302b1f278f05e4083e1d
neoruby <admin@azura.cfx.kr>
2026-08-07 15:58:51 +0900

    chore(db): Prisma 마이그레이션 단일화

--- prisma/migrations/20260802091623_add_post_author_board_indexes/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "Post_authorId_deletedAt_idx" ON "Post"("authorId", "deletedAt");
-
--- CreateIndex
-CREATE INDEX "Post_boardId_deletedAt_createdAt_idx" ON "Post"("boardId", "deletedAt", "createdAt");
--- prisma/migrations/20260807130000_add_notice_board_option/migration.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "Board" ADD COLUMN "isNoticeBoard" BOOLEAN NOT NULL DEFAULT false;

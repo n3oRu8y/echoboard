8242ff86d2b7e1848ef8ddf37d6f7225945c8761
neoruby <admin@azura.cfx.kr>
2026-08-23 21:03:57 +0900

    fix(middleware): 404 오류페이지 응답코드 200 문제 수정

--- src/middlewares/PageNotFound.ts
+++ src/middlewares/PageNotFound.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
 
 export default function PageNotFound(req: Request, res: Response, next: NextFunction) {
-    return res.render("errors/404.ejs");
+    return res.status(404).render("errors/404.ejs");
 }
